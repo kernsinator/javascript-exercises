403449421bdbc0ba2d5302994ab3af4bd8802752
Tyler Kerns <tkerns@protonmail.com>
2023-10-05 13:18:22 -0500

    Complete first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if (repeats < 0) {
+        return "ERROR";
+    }
+    
+    let result = "";
+    for (i = 0; i < repeats; i++){
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
